e511fb8de0b4cc70ab51347ba9d279bf46cbe0eb
Jackson-Greene <Jackson.Greene@student.curtin.edu.au>
2021-07-11 23:03:02 +0800

    working query to return tasks json

--- api/TestApiHandler.py
+++ api/TestApiHandler.py
@@ -7,8 +7,10 @@ user_tasks_collection = database.get_collection("user_tasks")
 
 class TestApiHandler(Resource):
   def get(self):
-    
-    return(user_tasks_collection.find_one())
+    tasks_from_db = user_tasks_collection.find_one({"_id": 0, "dates.date": "12072021"}, {"dates.tasks.$": 1})
+    json_to_return = {}
+    json_to_return["tasks"] = tasks_from_db["dates"]
+    return(json_to_return)
 
   def post(self):
     print(self)
